40be7164ac78a88ec1017ebd501f91eb26fa070a
Daniel Stocks <daniel@webcloud.se>
2014-04-08 17:25:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-sortable
+
+This repository contains example code for creating a sortable component in React
+
+Read more about creating a sortable component from scratch: http://webcloud.se/sortable-list-component-react-js/
+
+# usage
+
+bower install
+python -m SimpleHTTPServer 8000
+open http://localhost:8000/
---
+++ src/SortableList.js
@@ -0,0 +1,52 @@
+/** @jsx React.DOM */
+
+var placeholder = document.createElement("li");
+placeholder.className = "placeholder";
+
+var SortableList = React.createClass({
+  getInitialState: function() {
+    return {data: this.props.data};
+  },
+  dragStart: function(e) {
+    this.dragged = e.currentTarget;
+    e.dataTransfer.effectAllowed = 'move';
+
+    // Firefox requires dataTransfer data to be set
+    e.dataTransfer.setData("text/html", e.currentTarget);
+  },
+  dragEnd: function(e) {
+
+    this.dragged.style.display = "block";
+    this.dragged.parentNode.removeChild(placeholder);
+
+    // Update data
+    var data = this.state.data;
+    var from = Number(this.dragged.dataset.id);
+    var to = Number(this.over.dataset.id);
+    if(from < to) to--;
+    data.splice(to, 0, data.splice(from, 1)[0]);
+    this.setState({data: data});
+  },
+  dragOver: function(e) {
+    e.preventDefault();
+    this.dragged.style.display = "none";
+    if(e.target.className == "placeholder") return;
+    this.over = e.target;
+    e.target.parentNode.insertBefore(placeholder, e.target);
+  },
+  render: function() {
+    var listItems = this.state.data.map((function(item, i) {
+      return (
+        <li data-id={i}
+            key={i}
+            draggable="true"
+            onDragEnd={this.dragEnd}
+            onDragStart={this.dragStart}>
+          {item}
+        </li>
+      );
+    }).bind(this));
+
+    return <ul onDragOver={this.dragOver}>{listItems}</ul>
+  }
+});
