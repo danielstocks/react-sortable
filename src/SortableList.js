9891ae2d53bedb20f1eb6fd4c566b6014b75865d
Daniel Stocks <daniel@webcloud.se>
2014-04-09 13:59:34 +0200

    Improved sortable interaction. Items can now be moved to the end of the list

--- src/SortableList.js
+++ src/SortableList.js
@@ -15,24 +15,41 @@ var SortableList = React.createClass({
     e.dataTransfer.setData("text/html", e.currentTarget);
   },
   dragEnd: function(e) {
-
     this.dragged.style.display = "block";
     this.dragged.parentNode.removeChild(placeholder);
-
-    // Update data
-    var data = this.state.data;
     var from = Number(this.dragged.dataset.id);
     var to = Number(this.over.dataset.id);
-    if(from < to) to--;
-    data.splice(to, 0, data.splice(from, 1)[0]);
-    this.setState({data: data});
+    if(this.nodePlacement == "after") to++;
+    this.update(from, to);
   },
   dragOver: function(e) {
+
     e.preventDefault();
     this.dragged.style.display = "none";
     if(e.target.className == "placeholder") return;
     this.over = e.target;
-    e.target.parentNode.insertBefore(placeholder, e.target);
+
+    // For a nice sortable experience
+    // we wan to be able to drop over
+    // or under the target element based
+    // on mouse position
+    var relY = e.clientY - this.over.offsetTop;
+    var height = this.over.offsetHeight / 2;
+
+    if(relY > height) {
+      this.nodePlacement = "after";
+      e.target.parentNode.insertBefore(placeholder, e.target.nextElementSibling);
+    }
+    else if(relY < height) {
+      this.nodePlacement = "before"
+      e.target.parentNode.insertBefore(placeholder, e.target);
+    }
+  },
+  update: function(from,to) {
+    var data = this.state.data;
+    if(from < to) to--;
+    data.splice(to, 0, data.splice(from, 1)[0]);
+    this.setState({data: data});
   },
   render: function() {
     var listItems = this.state.data.map((function(item, i) {
