1c3b116c98b53c1637d7786f44487c7fbb7caaa9
unknown <kkomai@LAB-D383.team-lab.local>
2015-11-12 15:41:22 +0900

    support React 14.x

--- src/Sortable.js
+++ src/Sortable.js
@@ -1,12 +1,24 @@
 var Sortable = {
   getDefaultProps: function() {
     return {
-      "data-id" : this.props.key,
-      draggable : true,
-      onDragEnd: this.sortEnd.bind(this),
-      onDragOver: this.dragOver.bind(this),
-      onDragStart: this.sortStart.bind(this)
+      draggable : true
     }
+  },
+  componentDidMount: function() {
+    var el = ReactDOM.findDOMNode(this);
+    el.dataset.id = this.props.sortId;
+    el.setAttribute('draggable', true);
+    el.setAttribute('data-id', this.props.sortId);
+    el.ondragend = this.sortEnd;
+    el.ondragover = this.dragOver;
+    el.ondragstart = this.sortStart;
+  },
+  componentWillUnmount: function() {
+    var el = ReactDOM.findDOMNode(this);
+    el.removeAttribute('data-id');
+    el.ondragend = null;
+    el.ondragover = null;
+    el.ondragstart = null;
   },
   update: function(to, from) {
     var data = this.props.data.items;
@@ -30,22 +42,23 @@ var Sortable = {
   move: function(over,append) {
     var to = Number(over.dataset.id);
     var from = this.props.data.dragging != undefined ? this.props.data.dragging : Number(this.dragged);
+
     if(append) to++;
     if(from < to) to--;
     this.update(to,from);
   },
   dragOver: function(e) {
     e.preventDefault();
-    var over = e.currentTarget
+    var over = e.currentTarget;
     var relX = e.clientX - over.getBoundingClientRect().left;
     var relY = e.clientY - over.getBoundingClientRect().top;
     var height = over.offsetHeight / 2;
-    var placement = this.placement ? this.placement(relX, relY, over) : relY > height
+    var placement = this.placement ? this.placement(relX, relY, over) : relY > height;
     this.move(over, placement);
   },
   isDragging: function() {
-    return this.props.data.dragging == this.props.key
+    return this.props.data.dragging == this.props.sortId;
   }
-}
+};
 
 module.exports = Sortable;
