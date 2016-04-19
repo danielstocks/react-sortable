58126c6b5e32cd16ddfd079a34f9355445d25bf0
Ladislav Maxa <ladislavmaxa@outlook.com>
2016-04-20 01:53:54 +0200

    SortableNested commented out

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 module.exports.Sortable = require('./src/SortableComposition.js');
-module.exports.SortableNested = require('./src/SortableNested.js');
+//module.exports.SortableNested = require('./src/SortableNested.js');
