0cce5dd73e0824643e2f9a121d028455dd39eb1d
Andrew Hutchings <andrew.hutchings@theice.com>
2016-06-06 11:55:41 -0400

    Generate transpiled sources before publishing to npm
    * Add a prepublish script that transpiles src/ into lib/
    * Add lib and src to package.json "files" field so that they are uploaded to the npm registry
    * Point the main field to lib/index.js for non-ES2015 consumers (node, webpack, browserify)
    * Add jsnext:main field that points to the ES2015 version of the source for rollup and other ES2015-consuming bundlers (https://github.com/rollup/rollup/wiki/jsnext:main)
    * Remove browserify configuration that is no longer needed since the transpile happens at publish time

--- index.js
+++
@@ -1,3 +0,0 @@
-import { SortableComposition } from './src/SortableComposition';
-export default SortableComposition;
-//module.exports.SortableComposition = require('./src/SortableComposition.js');
