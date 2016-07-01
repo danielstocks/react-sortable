b91b3b8776b8941f00bc95afaa1f110afe0383fa
Ladislav Maxa <dharmoslap@gmail.com>
2016-07-01 17:48:47 +0200

    demo update

--- demo/StateView.js
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-var StateView = React.createClass({
-  render: function() {
-    return (
-      <pre>{JSON.stringify(this.props.data, 0, 2)}</pre>
-    )
-  }
-})
--- demo/gridlist/SortableListItem.js
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-var SortableListItem = React.createClass({
-  mixins: [Sortable],
-  render: function() {
-    return this.transferPropsTo(
-      <li className={this.isDragging() ? "dragging" : ""}>{this.props.item}</li>
-    );
-  }
-})
